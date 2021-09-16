976d59cfc030f0813f760a4a8a8356ff9765732f
kajalbhardwaj <kabharadwaj@bestpeers.com>
2021-09-16 20:02:37 +0530

    blog app

---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+#Rails.application.routes.draw do
+  #get "/articles", to: "articles#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
+
+#Rails.application.routes.draw do
+  #root "articles#index"
+
+ # get "/articles", to: "articles#index"
+#end
+#Rails.application.routes.draw do
+  #root "articles#index"
+
+  #get "/articles", to: "articles#index"
+ # get "/articles/:id", to: "articles#show"
+#end
+#Rails.application.routes.draw do
+  #root "articles#index"
+
+  #resources :articles
+#end
+
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
